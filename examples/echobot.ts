b9ea7ca5ddb3a00c6f3656555e41cdd6cd4e818a
RFS-ADRENO <dungto76@gmail.com>
2024-08-13 20:02:03 +0700

    example: added echobot

---
+++ examples/echobot.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import { J2Cookies, Zalo } from "../src/index.js";
+import { MessageType } from "../src/models/Message.js";
+const zalo = new Zalo({
+    cookie: JSON.parse(fs.readFileSync("./examples/cookies.json", "utf-8")) as J2Cookies,
+    imei: "",
+    userAgent: ""
+});
+
+const api = await zalo.login();
+const { listener } = api;
+
+listener.on("message", (message) => {
+    const isDirectMessage = message.type === MessageType.DirectMessage;
+
+    if (isDirectMessage && !message.isSelf) {
+        console.log("Message:", message.data.content, message.threadId);
+        api.sendMessage(message.data.content, message.threadId, message);
+    }
+});
+
+listener.onConnected(() => {
+    console.log("Connected");
+});
+
+listener.onClosed(() => {
+    console.log("Closed");
+});
+
+listener.onError((error: any) => {
+    console.error("Error:", error);
+});
+
+listener.start();
