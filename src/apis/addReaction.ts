3822107f2469e6d3ec19ba1eaf107008a490c1d1
JustKemForFun <haocong728@gmail.com>
2024-10-26 23:12:54 +0700

    feat(api): add `Reactions`

--- src/apis/addReaction.ts
+++ src/apis/addReaction.ts
@@ -50,6 +50,198 @@ export function addReactionFactory(serviceURL: string) {
                 rType = 20;
                 source = 6;
                 break;
+            case Reactions.KISS:
+                rType = 8;
+                source = 6;
+                break;
+            case Reactions.TEARS_OF_JOY:
+                rType = 7;
+                source = 6;
+                break;
+            case Reactions.SHIT:
+                rType = 66;
+                source = 6;
+                break;
+            case Reactions.ROSE:
+                rType = 120;
+                source = 6;
+                break;
+            case Reactions.BROKEN_HEART:
+                rType = 65;
+                source = 6;
+                break;
+            case Reactions.DISLIKE:
+                rType = 4;
+                source = 6;
+                break;
+            case Reactions.LOVE:
+                rType = 29;
+                source = 6;
+                break;
+            case Reactions.CONFUSED:
+                rType = 51;
+                source = 6;
+                break;
+            case Reactions.WINK:
+                rType = 45;
+                source = 6;
+                break;
+            case Reactions.FADE:
+                rType = 121;
+                source = 6;
+                break;
+            case Reactions.SUN:
+                rType = 67;
+                source = 6;
+                break;
+            case Reactions.BIRTHDAY:
+                rType = 126;
+                source = 6;
+                break;
+            case Reactions.BOMB:
+                rType = 127;
+                source = 6;
+                break;
+            case Reactions.OK:
+                rType = 68;
+                source = 6;
+                break;
+            case Reactions.PEACE:
+                rType = 69;
+                source = 6;
+                break;
+            case Reactions.THANKS:
+                rType = 70;
+                source = 6;
+                break;
+            case Reactions.PUNCH:
+                rType = 71;
+                source = 6;
+                break;
+            case Reactions.SHARE:
+                rType = 72;
+                source = 6;
+                break;
+            case Reactions.PRAY:
+                rType = 73;
+                source = 6;
+                break;
+            case Reactions.NO:
+                rType = 131;
+                source = 6;
+                break;
+            case Reactions.BAD:
+                rType = 132;
+                source = 6;
+                break;
+            case Reactions.LOVE_YOU:
+                rType = 133;
+                source = 6;
+                break;
+            case Reactions.SAD:
+                rType = 1;
+                source = 6;
+                break;
+            case Reactions.VERY_SAD:
+                rType = 16;
+                source = 6;
+                break;
+            case Reactions.COOL:
+                rType = 21;
+                source = 6;
+                break;
+            case Reactions.NERD:
+                rType = 22;
+                source = 6;
+                break;
+            case Reactions.BIG_SMILE:
+                rType = 23;
+                source = 6;
+                break;
+            case Reactions.SUNGLASSES:
+                rType = 26;
+                source = 6;
+                break;
+            case Reactions.NEUTRAL:
+                rType = 30;
+                source = 6;
+                break;
+            case Reactions.SAD_FACE:
+                rType = 35;
+                source = 6;
+                break;
+            case Reactions.BYE:
+                rType = 36;
+                source = 6;
+                break;
+            case Reactions.SLEEPY:
+                rType = 38;
+                source = 6;
+                break;
+            case Reactions.WIPE:
+                rType = 39;
+                source = 6;
+                break;
+            case Reactions.DIG:
+                rType = 42;
+                source = 6;
+                break;
+            case Reactions.ANGUISH:
+                rType = 44;
+                source = 6;
+                break;
+            case Reactions.HANDCLAP:
+                rType = 46;
+                source = 6;
+                break;
+            case Reactions.ANGRY_FACE:
+                rType = 47;
+                source = 6;
+                break;
+            case Reactions.F_CHAIR:
+                rType = 48;
+                source = 6;
+                break;
+            case Reactions.L_CHAIR:
+                rType = 49;
+                source = 6;
+                break;
+            case Reactions.R_CHAIR:
+                rType = 50;
+                source = 6;
+                break;
+            case Reactions.SILENT:
+                rType = 52;
+                source = 6;
+                break;
+            case Reactions.SURPRISE:
+                rType = 53;
+                source = 6;
+                break;
+            case Reactions.EMBARRASSED:
+                rType = 54;
+                source = 6;
+                break;
+            case Reactions.AFRAID:
+                rType = 60;
+                source = 6;
+                break;
+            case Reactions.SAD2:
+                rType = 61;
+                source = 6;
+                break;
+            case Reactions.BIG_LAUGH:
+                rType = 62;
+                source = 6;
+                break;
+            case Reactions.RICH:
+                rType = 63;
+                source = 6;
+                break;
+            case Reactions.BEER:
+                rType = 99;
+                source = 6;
+                break;
             default:
                 rType = -1;
                 source = 6;
