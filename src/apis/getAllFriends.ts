5eae6bd5dfed16d974db1498cd9eee15c9dbb662
RFS-ADRENO <dungto76@gmail.com>
2025-03-25 10:11:27 +0700

    feat(api): support count & page for getAllFriends api (close #72)

--- src/apis/getAllFriends.ts
+++ src/apis/getAllFriends.ts
@@ -1,3 +1,4 @@
+import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { apiFactory } from "../utils.js";
 
 export type GetAllFriendsResponse = {
@@ -36,13 +37,38 @@ export type GetAllFriendsResponse = {
     oa_status: any;
 }[];
 
-export const getAllFriendsFactory = apiFactory<GetAllFriendsResponse>()((api, _, utils) => {
+export const getAllFriendsFactory = apiFactory<GetAllFriendsResponse>()((api, ctx, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.profile[0]}/api/social/friend/getfriends`);
 
-    return async function getAllFriends() {
-        const response = await utils.request(serviceURL, {
-            method: "GET",
-        });
+    /**
+     * Get all friends
+     * 
+     * @param count Page size (default: 20000)
+     * @param page Page number (default: 1)
+     * 
+     * @throws ZaloApiError
+     */
+    return async function getAllFriends(count: number = 20000, page: number = 1) {
+        const params = {
+            incInvalid: 1,
+            page,
+            count,
+            avatar_size: 120,
+            actiontime: 0,
+            imei: ctx.imei,
+        };
+
+        const encryptedParams = utils.encodeAES(JSON.stringify(params));
+        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt message");
+
+        const response = await utils.request(
+            utils.makeURL(serviceURL, {
+                params: encryptedParams,
+            }),
+            {
+                method: "GET",
+            },
+        );
 
         return utils.resolve(response);
     };
