66c9ae76baca9643425d33b6dcc6881cbf0efdd9
JustKemForFun <haocong728@gmail.com>
2025-06-03 15:34:14 +0700

    feat(api): enhance getListReminder API to support user and group threads
    - Updated the getListReminder API to differentiate between user and group threads using ThreadType.
    - Modified request parameters to include threadId and type, improving flexibility in retrieving reminders.
    - Adjusted service URL construction based on thread type for more accurate API calls.

--- src/apis/getListReminder.ts
+++ src/apis/getListReminder.ts
@@ -1,4 +1,5 @@
 import { ZaloApiError } from "../Errors/ZaloApiError.js";
+import { ThreadType } from "../models/index.js";
 import { apiFactory } from "../utils.js";
 
 export type ListReminderParams = {
@@ -44,34 +45,53 @@ export type ReminderItem = {
 export type GetListReminderResponse = ReminderItem[];
 
 export const getListReminderFactory = apiFactory<GetListReminderResponse>()((api, ctx, utils) => {
-    const serviceURL = utils.makeURL(`${api.zpwServiceMap.group_board[0]}/api/board/listReminder`);
+    const serviceURL = {
+        [ThreadType.User]: utils.makeURL(`${api.zpwServiceMap.group_board[0]}/api/board/oneone/list`),
+        [ThreadType.Group]: utils.makeURL(`${api.zpwServiceMap.group_board[0]}/api/board/listReminder`),
+    };
 
     /**
      * Get list reminder
      *
      * @param params - The parameters for the request
-     * @param groupId - The ID of the group
+     * @param threadId - The ID of the thread
+     * @param type - The type of the thread (User or Group)
      *
      * @throws ZaloApiError
      *
      */
-    return async function getListReminder(params: ListReminderParams, groupId: string) {
+    return async function getListReminder(
+        params: ListReminderParams,
+        threadId: string,
+        type: ThreadType = ThreadType.User,
+    ) {
         const requestParams = {
-            objectData: JSON.stringify({
-                group_id: groupId,
-                board_type: 1,
-                page: params.page || 1,
-                count: params.count || 20,
-                last_id: 0,
-                last_type: 0,
-            }),
-            imei: ctx.imei,
+            objectData: JSON.stringify(
+                type === ThreadType.User
+                    ? {
+                          uid: threadId,
+                          board_type: 1,
+                          page: params.page || 1,
+                          count: params.count || 20,
+                          last_id: 0,
+                          last_type: 0,
+                      }
+                    : {
+                          group_id: threadId,
+                          board_type: 1,
+                          page: params.page || 1,
+                          count: params.count || 20,
+                          last_id: 0,
+                          last_type: 0,
+                      },
+            ),
+            ...(type === ThreadType.Group && { imei: ctx.imei }),
         };
 
         const encryptedParams = utils.encodeAES(JSON.stringify(requestParams));
         if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
 
-        const response = await utils.request(utils.makeURL(serviceURL, { params: encryptedParams }), {
+        const response = await utils.request(utils.makeURL(serviceURL[type].toString(), { params: encryptedParams }), {
             method: "GET",
         });
 
