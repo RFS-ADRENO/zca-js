3341fdb82bab1e35872894e81dc816f97e45f310
JustKemForFun <haocong728@gmail.com>
2025-06-04 16:24:07 +0700

    feat(api): add join and leave group functionalities; resolve (#127)
    - Introduced new APIs for joining and leaving groups, including request handling and response types.
    - Updated index and Zalo class to integrate the new joinGroup and leaveGroup methods.
    - Added types for JoinGroupResponse, LeaveGroupResponse, and LinkGroupInfoResponse to enhance type safety.

---
+++ src/apis/joinGroup.ts
@@ -0,0 +1,31 @@
+import { ZaloApiError } from "../Errors/ZaloApiError.js";
+import { apiFactory } from "../utils.js";
+
+export type JoinGroupResponse = "";
+
+export const joinGroupFactory = apiFactory<JoinGroupResponse>()((api, ctx, utils) => {
+    const serviceURL = utils.makeURL(`${api.zpwServiceMap.group[0]}/api/group/link/join`);
+
+    /**
+     * Join group
+     *
+     * @param link - The link join group
+     *
+     * @throws ZaloApiError
+     */
+    return async function joinGroup(link: string) {
+        const params = {
+            link: link,
+            clientLang: ctx.language,
+        };
+
+        const encryptedParams = utils.encodeAES(JSON.stringify(params));
+        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+        const response = await utils.request(utils.makeURL(serviceURL, { params: encryptedParams }), {
+            method: "GET",
+        });
+
+        return utils.resolve(response);
+    };
+});
---
+++ src/apis/leaveGroup.ts
@@ -0,0 +1,38 @@
+import { ZaloApiError } from "../Errors/ZaloApiError.js";
+import { apiFactory } from "../utils.js";
+
+export type LeaveGroupResponse = {
+    memberError: string[];
+};
+
+export const leaveGroupFactory = apiFactory<LeaveGroupResponse>()((api, ctx, utils) => {
+    const serviceURL = utils.makeURL(`${api.zpwServiceMap.group[0]}/api/group/leave`);
+
+    /**
+     * Leave group
+     *
+     * @param groupId - The ID of the group(s) to leave
+     *
+     * @throws ZaloApiError
+     */
+    return async function leaveGroup(groupId: string | string[], silent: boolean = false) {
+        const requestParams = {
+            grids: Array.isArray(groupId) ? groupId : [groupId],
+            imei: ctx.imei,
+            silent: silent ? 1 : 0,
+            language: ctx.language,
+        };
+
+        const encryptedParams = utils.encodeAES(JSON.stringify(requestParams));
+        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt params");
+
+        const response = await utils.request(serviceURL, {
+            method: "POST",
+            body: new URLSearchParams({
+                params: encryptedParams,
+            }),
+        });
+
+        return utils.resolve(response);
+    };
+});
