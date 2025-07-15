f41e6f919dc1f03e5fff2bc3f9d116bbfcbf94c5
RFS-ADRENO <dungto76@gmail.com>
2025-07-15 23:17:17 +0700

    docs(leaveGroup): clarify error behavior

--- src/apis/leaveGroup.ts
+++ src/apis/leaveGroup.ts
@@ -13,6 +13,8 @@ export const leaveGroupFactory = apiFactory<LeaveGroupResponse>()((api, ctx, uti
      *
      * @param groupId - The ID of the group(s) to leave
      * @param silent - Turn on/off silent leave group
+     * 
+     * @note Zalo might throw an error with code 166 if you are not a member of the group
      *
      * @throws ZaloApiError
      */
