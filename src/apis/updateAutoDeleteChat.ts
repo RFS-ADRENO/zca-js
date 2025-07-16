7c281625f51152cc5b637d5c3e9d92c7ca4fea81
RFS-ADRENO <dungto76@gmail.com>
2025-07-16 20:02:49 +0700

    refactor(updateAutoDeleteChat): optimize logic; rename enum/params for clarity; remove duplicate one (autoDeleteChat)

--- src/apis/updateAutoDeleteChat.ts
+++ src/apis/updateAutoDeleteChat.ts
@@ -2,26 +2,37 @@ import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { ThreadType } from "../models/index.js";
 import { apiFactory } from "../utils.js";
 
+export enum ChatTTL {
+    NO_DELETE = 0,
+    ONE_DAY = 86400000,
+    SEVEN_DAYS = 604800000,
+    FOURTEEN_DAYS = 1209600000,
+}
+
 export type UpdateAutoDeleteChatResponse = "";
 
 export const updateAutoDeleteChatFactory = apiFactory<UpdateAutoDeleteChatResponse>()((api, ctx, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.conversation[0]}/api/conv/autodelete/updateConvers`);
 
     /**
-     * Update auto delete chat
-     * 
-     * @param ttl - The time to live for the autoDeleteMessage of API
-     * @param threadId - The ID of the thread to update
-     * @param isGroup - Whether the thread is a group
+     * Auto delete chat
+     *
+     * @param ttl The time to live of the chat
+     * @param threadId The thread ID to auto delete chat
+     * @param type Type of thread (User or Group)
      *
      * @throws ZaloApiError
      */
-    return async function updateAutoDeleteChat(ttl: number = 0, threadId: string, isGroup: ThreadType = ThreadType.Group) {
+    return async function updateAutoDeleteChat(
+        ttl: ChatTTL,
+        threadId: string,
+        type: ThreadType = ThreadType.User,
+    ) {
         const params = {
-            "threadId": threadId,
-            "isGroup": isGroup === ThreadType.Group ? 1 : 0,
-            "ttl": ttl, // time update autoDeleteMessage
-            "clientLang": ctx.language
+            threadId: threadId,
+            isGroup: type === ThreadType.Group ? 1 : 0,
+            ttl: ttl,
+            clientLang: ctx.language,
         };
 
         const encryptedParams = utils.encodeAES(JSON.stringify(params));
