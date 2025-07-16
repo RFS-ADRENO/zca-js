2c8144dcf842d8e395cfdbfceaed462a2de46510
RFS-ADRENO <dungto76@gmail.com>
2025-07-16 16:29:48 +0700

    docs(sendFriendRequest): clarify error behavior

--- src/apis/sendFriendRequest.ts
+++ src/apis/sendFriendRequest.ts
@@ -12,6 +12,10 @@ export const sendFriendRequestFactory = apiFactory<SendFriendRequestResponse>()(
      * @param msg message sent with friend request
      * @param userId User ID to send friend request to
      *
+     * @note Zalo might throw an error with code 225 if the user is already friends with you,
+     *          215 if the user might have blocked you,
+     *          222 if user has already sent you a friend request, your request will be treated as acceptance instead.
+     *
      * @throws ZaloApiError
      */
     return async function sendFriendRequest(msg: string, userId: string) {
