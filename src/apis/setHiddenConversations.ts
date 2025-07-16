db4134d033c66c834ff1bc4e1d97c88cb8246fdd
RFS-ADRENO <dungto76@gmail.com>
2025-07-16 11:11:46 +0700

    refactor(api)!: merge add/remove hidden conversation API into setHiddenConversations

--- src/apis/setHiddenConversations.ts
+++ src/apis/setHiddenConversations.ts
@@ -2,28 +2,29 @@ import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { ThreadType } from "../models/index.js";
 import { apiFactory } from "../utils.js";
 
-export type AddHiddenConversationResponse = "";
+export type SetHiddenConversationsResponse = "";
 
-export const addHiddenConversationFactory = apiFactory<AddHiddenConversationResponse>()((api, ctx, utils) => {
+export const setHiddenConversationsFactory = apiFactory<SetHiddenConversationsResponse>()((api, ctx, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.conversation[0]}/api/hiddenconvers/add-remove`);
 
     /**
-     * Add hidden conversation
+     * Set hidden conversation
      *
+     * @param hidden - Whether to hide or unhide the conversation
      * @param threadId Thread ID
      * @param type Thread type (User/Group)
      *
      * @throws ZaloApiError
      */
-    return async function addHiddenConversation(threadId: string, type: ThreadType = ThreadType.User) {
+    return async function setHiddenConversations(hidden: boolean, threadId: string, type: ThreadType = ThreadType.User) {
         const params = {
-            del_threads: "[]",
-            add_threads: JSON.stringify([
+            [hidden ? "add_threads" : "del_threads"]: JSON.stringify([
                 {
                     thread_id: threadId,
                     is_group: type === ThreadType.Group ? 1 : 0,
                 },
             ]),
+            [hidden ? "del_threads" : "add_threads"]: "[]",
             imei: ctx.imei,
         };
 
