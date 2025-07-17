908ff2cbb44e37760c54940c4745ab91eb0f3aa3
RFS-ADRENO <dungto76@gmail.com>
2025-07-17 15:08:40 +0700

    refactor(updatetLang): using enum for language input; clarify API bahavior

--- src/apis/updateLang.ts
+++ src/apis/updateLang.ts
@@ -1,19 +1,26 @@
 import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { apiFactory } from "../utils.js";
 
+export enum UpdateLangAvailableLanguages {
+    VI = "VI",
+    EN = "EN",
+}
+
 export type UpdateLangResponse = "";
 
-export const updateLangFactory = apiFactory<UpdateLangResponse>()((api, _ctx, utils) => {
+export const updateLangFactory = apiFactory<UpdateLangResponse>()((api, _, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.profile[0]}/api/social/profile/updatelang`);
 
     /**
-     * Update language
+     * Update language?
      *
      * @param language language to update (VI, EN)
+     * 
+     * @note Calling this API alone will not update the user's language.
      *
      * @throws ZaloApiError
      */
-    return async function updateLang(language: string = "VI") {
+    return async function updateLang(language: UpdateLangAvailableLanguages = UpdateLangAvailableLanguages.VI) {
         const params = {
             language: language,
         };
