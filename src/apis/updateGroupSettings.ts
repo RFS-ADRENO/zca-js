9458ec96784022ed4bbbbdaac3f98d84840a7900
RFS-ADRENO <dungto76@gmail.com>
2025-07-17 10:15:53 +0700

    refactor(updateGroupSettings)!: correct logic; detailed docs

--- src/apis/updateGroupSettings.ts
+++ src/apis/updateGroupSettings.ts
@@ -2,23 +2,50 @@ import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { apiFactory } from "../utils.js";
 
 export type UpdateGroupSettingsOptions = {
-    blockName?: boolean; // thay doi ten va anh dai dien nhom
-    signAdminMsg?: boolean; // danh dau tin nhan tu truong/pho nhom
-    addMemberOnly?: boolean; // chi admin moi them duoc thanh vien
-    setTopicOnly?: boolean; // ghim tin nhan, ghi chu, binh chon len dau hoi thoai
-    enableMsgHistory?: boolean; // cho phep thanh vien moi doc lai tin nhan gan nhat
-    joinAppr?: boolean; // che do phe duyet thanh vien moi
-    lockCreatePost?: boolean; // tao moi ghi chu, nhac hen
-    lockCreatePoll?: boolean; // tao moi binh chon
-    lockSendMsg?: boolean; // gui tin nhan
+    /**
+     * Disallow group members to change the group name and avatar
+     */
+    blockName?: boolean;
+    /**
+     * Highlight messages from owner/admins
+     */
+    signAdminMsg?: boolean;
+    /**
+     * Don't pin messages, notes, and polls to the top of a conversation
+     */
+    setTopicOnly?: boolean;
+    /**
+     * Allow new members to read most recent messages
+     */
+    enableMsgHistory?: boolean;
+    /**
+     * Membership approval
+     */
+    joinAppr?: boolean;
+    /**
+     * Disallow group members to create notes & reminders
+     */
+    lockCreatePost?: boolean;
+    /**
+     * Disallow group members to create polls
+     */
+    lockCreatePoll?: boolean;
+    /**
+     * Disallow group members to send messages
+     */
+    lockSendMsg?: boolean;
+    /**
+     * Disallow group members to view full member list (community only)
+     */
+    lockViewMember?: boolean;
 
-    lockViewMember?: boolean; // tat het bat chi truong/pho nhom nhan 
-    bannFeature?: boolean; // tat ghim tin nhan, ghi chu, binh chon len dau hoi thoai va cho phep thanh vien doc lai tin nhan gan nhat
-    // dirtyMedia?: boolean; // khong cho thanh vien gui anh, video
-    // banDuration?: boolean | number; // thoi gian ban thanh vien
+    // bannFeature?: boolean; // not see in UI, not implemented
+    // addMemberOnly?: boolean; // not see in UI, not implemented
+    // dirtyMedia?: boolean; // not see in UI, not implemented
+    // banDuration?: boolean | number; // not see in UI, not implemented
 };
 
-export type UpdateGroupSettingsResponse = {};
+export type UpdateGroupSettingsResponse = "";
 
 export const updateGroupSettingsFactory = apiFactory<UpdateGroupSettingsResponse>()((api, ctx, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.group[0]}/api/group/setting/update`);
@@ -29,25 +56,31 @@ export const updateGroupSettingsFactory = apiFactory<UpdateGroupSettingsResponse
      * @param options Options
      * @param groupId Group ID
      *
+     * @note Zalo might throw an error with code 166 if you don't have enough permissions to change the settings.
+     *
      * @throws ZaloApiError
      */
     return async function updateGroupSettings(options: UpdateGroupSettingsOptions, groupId: string) {
         const params = {
-            blockName: options.blockName ? 0 : 1,
+            blockName: options.blockName ? 1 : 0,
             signAdminMsg: options.signAdminMsg ? 1 : 0,
-            addMemberOnly: options.addMemberOnly ? 0 : 1,
-            setTopicOnly: options.setTopicOnly ? 0 : 1,
+
+            // addMemberOnly: 0, // very tricky, any idea?
+
+            setTopicOnly: options.setTopicOnly ? 1 : 0,
             enableMsgHistory: options.enableMsgHistory ? 1 : 0,
             joinAppr: options.joinAppr ? 1 : 0,
-            lockCreatePost: options.lockCreatePost ? 0 : 1,
-            lockCreatePoll: options.lockCreatePoll ? 0 : 1,
-            lockSendMsg: options.lockSendMsg ? 0 : 1,
+            lockCreatePost: options.lockCreatePost ? 1 : 0,
+            lockCreatePoll: options.lockCreatePoll ? 1 : 0,
+            lockSendMsg: options.lockSendMsg ? 1 : 0,
+            lockViewMember: options.lockViewMember ? 1 : 0,
 
-            lockViewMember: options.lockViewMember ? 1: 0,
-            bannFeature: options.bannFeature ? 1 : 0,
-            // dirtyMedia: options.dirtyMedia ? 1 : 0, // not see
-            // banDuration: options.banDuration ? 1 : 0, // not see
-            // blocked_members: [], not implemented
+            // default values for not implemented options
+            bannFeature: 0,
+            dirtyMedia: 0,
+            banDuration: 0,
+            blocked_members: [],
+
             grid: groupId,
             imei: ctx.imei,
         };
