e8fedd647e8bde40b37318d19df6a150e9a40d1f
JustKemForFun <haocong728@gmail.com>
2025-08-21 22:34:15 +0700

    feat(api): enhance getBizAccount and updateProfile to use BusinessCategory type and add new fields for business profiles

--- src/models/ZBusiness.ts
+++ src/models/ZBusiness.ts
@@ -2,3 +2,46 @@ export type ZBusinessPackage = {
     label?: Record<string, string> | null;
     pkgId: number; // @TODO: what is this?
 };
+
+export enum BusinessCategory {
+    Other = 0,
+    RealEstate = 1,
+    TechnologyAndDevices = 2,
+    TravelAndHospitality = 3,
+    EducationAndTraining = 4,
+    ShoppingAndRetail = 5,
+    CosmeticsAndBeauty = 6,
+    RestaurantAndCafe = 7,
+    AutoAndMotorbike = 8,
+    FashionAndApparel = 9,
+    FoodAndBeverage = 10,
+    MediaAndEntertainment = 11,
+    InternalCommunications = 12,
+    Transportation = 13,
+    Telecommunications = 14,
+}
+
+/**
+ * eg.
+   import { BusinessCategory, BusinessCategoryName } from "../models";
+
+   const cate = BusinessCategory.FoodAndBeverage; // 10
+   const label = BusinessCategoryName[cate]; // "Thực phẩm & Đồ uống"
+ */
+export const BusinessCategoryName: Record<BusinessCategory, string> = {
+    [BusinessCategory.Other]: "Dịch vụ khác (Không hiển thị)",
+    [BusinessCategory.RealEstate]: "Bất động sản",
+    [BusinessCategory.TechnologyAndDevices]: "Công nghệ & Thiết bị",
+    [BusinessCategory.TravelAndHospitality]: "Du lịch & Lưu trú",
+    [BusinessCategory.EducationAndTraining]: "Giáo dục & Đào tạo",
+    [BusinessCategory.ShoppingAndRetail]: "Mua sắm & Bán lẻ",
+    [BusinessCategory.CosmeticsAndBeauty]: "Mỹ phẩm & Làm đẹp",
+    [BusinessCategory.RestaurantAndCafe]: "Nhà hàng & Quán",
+    [BusinessCategory.AutoAndMotorbike]: "Ô tô & Xe máy",
+    [BusinessCategory.FashionAndApparel]: "Thời trang & May mặc",
+    [BusinessCategory.FoodAndBeverage]: "Thực phẩm & Đồ uống",
+    [BusinessCategory.MediaAndEntertainment]: "Truyền thông & Giải trí",
+    [BusinessCategory.InternalCommunications]: "Truyền thông nội bộ",
+    [BusinessCategory.Transportation]: "Vận tải",
+    [BusinessCategory.Telecommunications]: "Viễn thông",
+};
