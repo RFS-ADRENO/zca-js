9f428a11e817a35267000e5abb95246ce6d9a133
JustKemForFun <haocong728@gmail.com>
2025-08-23 12:52:13 +0700

    feat(api): enhance addQuickMessage and updateQuickMessage to support media handling and update QuickMessage model structure

--- src/models/QuickMessage.ts
+++ src/models/QuickMessage.ts
@@ -1,12 +1,28 @@
 export type QuickMessage = {
-    id: number;
-    keyword: string;
-    type: number;
-    createdTime: number;
-    lastModified: number;
-    message: {
-        title: string;
-        params: string | null;
+    item: {
+        id: number;
+        keyword: string;
+        type: number;
+        createdTime: number;
+        lastModified: number;
+        message: {
+            title: string;
+            params: string | null;
+        };
+        media?: {
+            items: {
+                type: number;
+                photoId: number;
+                title: string;
+                width: number;
+                height: number;
+                previewThumb: string;
+                rawUrl: string;
+                thumbUrl: string;
+                normalUrl: string;
+                hdUrl: string;
+            }[];
+        } | null;
     };
-    media: null;
+    version: number;
 };
