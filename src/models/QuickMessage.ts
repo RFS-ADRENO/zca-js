aea0e59a027d42d761697d102b0c2f285b861d77
JustKemForFun <haocong728@gmail.com>
2025-08-29 16:40:13 +0700

    refactor(QuickMessage): simplify structure by removing unnecessary nesting in the QuickMessage type definition

--- src/models/QuickMessage.ts
+++ src/models/QuickMessage.ts
@@ -1,28 +1,26 @@
 export type QuickMessage = {
-    item: {
-        id: number;
-        keyword: string;
-        type: number;
-        createdTime: number;
-        lastModified: number;
-        message: {
-            title: string;
-            params: string | null;
-        };
-        media?: {
-            items: {
-                type: number;
-                photoId: number;
-                title: string;
-                width: number;
-                height: number;
-                previewThumb: string;
-                rawUrl: string;
-                thumbUrl: string;
-                normalUrl: string;
-                hdUrl: string;
-            }[];
-        } | null;
+    id: number;
+    keyword: string;
+    type: number;
+    createdTime: number;
+    lastModified: number;
+    message: {
+        title: string;
+        params: string | null;
     };
+    media?: {
+        items: {
+            type: number;
+            photoId: number;
+            title: string;
+            width: number;
+            height: number;
+            previewThumb: string;
+            rawUrl: string;
+            thumbUrl: string;
+            normalUrl: string;
+            hdUrl: string;
+        }[];
+    } | null;
     version: number;
 };
