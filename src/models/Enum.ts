8c14625a49e45626ed72b7840f8269a76bccdb40
RFS-ADRENO <dungto76@gmail.com>
2025-09-01 17:02:22 +0700

    docs(Enum): replace inline comments with block comments in BinBankCard enum

--- src/models/Enum.ts
+++ src/models/Enum.ts
@@ -32,61 +32,232 @@ export enum BoardType {
  * @documents https://developers.zalo.me/docs/zalo-notification-service/phu-luc/danh-sach-bin-code - docs missing bin code and short_name bank
  */
 export enum BinBankCard {
-    ABBank = 970425,                       // NH TMCP An Bình
-    ACB = 970416,                          // NH TMCP Á Châu
-    Agribank = 970405,                     // NH Nông nghiệp và Phát triển Nông thôn Việt Nam
-    BIDV = 970418,                         // NH TMCP Đầu tư và Phát triển Việt Nam
-    BVBank = 970454,                       // NH TMCP Bản Việt
-    BacA_Bank = 970409,                    // NH TMCP Bắc Á
-    BaoViet_Bank = 970438,                 // NH TMCP Bảo Việt
-    CAKE = 546034,                         // NH số CAKE by VPBank - TMCP Việt Nam Thịnh Vượng
-    CB_Bank = 970444,                      // NH Thương mại TNHH MTV Xây dựng Việt Nam
-    CIMB_Bank = 422589,                    // NH TNHH MTV CIMB Việt Nam
-    Coop_Bank = 970446,                    // NH Hợp tác xã Việt Nam
-    DBS_Bank = 796500,                     // NH TNHH MTV Phát triển Singapore - CN TP. Hồ Chí Minh
-    DongA_Bank = 970406,                   // NH TMCP Đông Á
-    Eximbank = 970431,                     // NH TMCP Xuất Nhập khẩu Việt Nam
-    GPBank = 970408,                       // NH TMCP Dầu khí Toàn cầu
-    HDBank = 970437,                       // NH TMCP Phát triển TP. Hồ Chí Minh
-    HSBC = 458761,                         // NH TNHH MTV HSBC (Việt Nam)
-    HongLeong_Bank = 970442,               // NH TNHH MTV Hong Leong Việt Nam
-    IBK_HCM = 970456,                      // NH Công nghiệp Hàn Quốc - CN TP. Hồ Chí Minh
-    IBK_HN = 970455,                       // NH Công nghiệp Hàn Quốc - CN Hà Nội
-    Indovina_Bank = 970434,                // NH TNHH Indovina
-    KBank = 668888,                        // NH Đại chúng TNHH Kasikornbank - CN TP. Hồ Chí Minh
-    KienlongBank = 970452,                 // NH TMCP Kiên Long
-    Kookmin_Bank_HCM = 970463,             // NH Kookmin - CN TP. Hồ Chí Minh
-    Kookmin_Bank_HN = 970462,              // NH Kookmin - CN Hà Nội
-    LPBank = 970449,                       // NH TMCP Lộc Phát Việt Nam
-    MB_Bank = 970422,                      // NH TMCP Quân đội
-    MSB = 970426,                          // NH TMCP Hàng Hải
-    NCB = 970419,                          // NH TMCP Quốc Dân
-    Nam_A_Bank = 970428,                   // NH TMCP Nam Á
-    NongHyup_Bank = 801011,                // NH Nonghyup - CN Hà Nội
-    OCB = 970448,                          // NH TMCP Phương Đông
-    Ocean_Bank = 970414,                   // NH Thương mại TNHH MTV Đại Dương
-    PGBank = 970430,                       // NH TMCP Thịnh vượng và Phát triển
-    PVcomBank = 970412,                    // NH TMCP Đại Chúng Việt Nam
-    Public_Bank_Vietnam = 970439,          // NH TNHH MTV Public Việt Nam
-    SCB = 970429,                          // NH TMCP Sài Gòn
-    SHB = 970443,                          // NH TMCP Sài Gòn - Hà Nội
-    Sacombank = 970403,                    // NH TMCP Sài Gòn Thương Tín
-    Saigon_Bank = 970400,                  // NH TMCP Sài Gòn Công Thương
-    SeABank = 970440,                      // NH TMCP Đông Nam Á
-    Shinhan_Bank = 970424,                 // NH TNHH MTV Shinhan Việt Nam
-    Standard_Chartered_Vietnam = 970410,   // NH TNHH MTV Standard Chartered Bank Việt Nam
-    TNEX = 9704261,                        // NH số TNEX
-    TPBank = 970423,                       // NH TMCP Tiên Phong
-    Techcombank = 970407,                  // NH TMCP Kỹ thương Việt Nam
-    Timo = 963388,                         // NH số Timo by Bản Việt Bank
-    UBank = 546035,                        // NH số UBank by VPBank
-    United_Overseas_Bank_Vietnam = 970458, // NH United Overseas Bank Việt Nam
-    VIB = 970441,                          // NH TMCP Quốc tế Việt Nam
-    VPBank = 970432,                       // NH TMCP Việt Nam Thịnh Vượng
-    VRB = 970421,                          // NH Liên doanh Việt - Nga
-    VietABank = 970427,                    // NH TMCP Việt Á
-    VietBank = 970433,                     // NH TMCP Việt Nam Thương Tín
-    Vietcombank = 970436,                  // NH TMCP Ngoại Thương Việt Nam
-    VietinBank = 970415,                   // NH TMCP Công thương Việt Nam
-    Woori_Bank = 970457,                   // NH TNHH MTV Woori Việt Nam
+    /**
+     * NH TMCP An Bình
+     */
+    ABBank = 970425,
+    /**
+     * NH TMCP Á Châu
+     */
+    ACB = 970416,
+    /**
+     * NH Nông nghiệp và Phát triển Nông thôn Việt Nam
+     */
+    Agribank = 970405,
+    /**
+     * NH TMCP Đầu tư và Phát triển Việt Nam
+     */
+    BIDV = 970418,
+    /**
+     * NH TMCP Bản Việt
+     */
+    BVBank = 970454,
+    /**
+     * NH TMCP Bắc Á
+     */
+    BacA_Bank = 970409,
+    /**
+     * NH TMCP Bảo Việt
+     */
+    BaoViet_Bank = 970438,
+    /**
+     * NH số CAKE by VPBank - TMCP Việt Nam Thịnh Vượng
+     */
+    CAKE = 546034,
+    /**
+     * NH Thương mại TNHH MTV Xây dựng Việt Nam
+     */
+    CB_Bank = 970444,
+    /**
+     * NH TNHH MTV CIMB Việt Nam
+     */
+    CIMB_Bank = 422589,
+    /**
+     * NH Hợp tác xã Việt Nam
+     */
+    Coop_Bank = 970446,
+    /**
+     * NH TNHH MTV Phát triển Singapore - CN TP. Hồ Chí Minh
+     */
+    DBS_Bank = 796500,
+    /**
+     * NH TMCP Đông Á
+     */
+    DongA_Bank = 970406,
+    /**
+     * NH TMCP Xuất Nhập khẩu Việt Nam
+     */
+    Eximbank = 970431,
+    /**
+     * NH TMCP Dầu khí Toàn cầu
+     */
+    GPBank = 970408,
+    /**
+     * NH TMCP Phát triển TP. Hồ Chí Minh
+     */
+    HDBank = 970437,
+    /**
+     * NH TNHH MTV HSBC (Việt Nam)
+     */
+    HSBC = 458761,
+    /**
+     * NH TNHH MTV Hong Leong Việt Nam
+     */
+    HongLeong_Bank = 970442,
+    /**
+     * NH Công nghiệp Hàn Quốc - CN TP. Hồ Chí Minh
+     */
+    IBK_HCM = 970456,
+    /**
+     * NH Công nghiệp Hàn Quốc - CN Hà Nội
+     */
+    IBK_HN = 970455,
+    /**
+     * NH TNHH Indovina
+     */
+    Indovina_Bank = 970434,
+    /**
+     * NH Đại chúng TNHH Kasikornbank - CN TP. Hồ Chí Minh
+     */
+    KBank = 668888,
+    /**
+     * NH TMCP Kiên Long
+     */
+    KienlongBank = 970452,
+    /**
+     * NH Kookmin - CN TP. Hồ Chí Minh
+     */
+    Kookmin_Bank_HCM = 970463,
+    /**
+     * NH Kookmin - CN Hà Nội
+     */
+    Kookmin_Bank_HN = 970462,
+    /**
+     * NH TMCP Lộc Phát Việt Nam
+     */
+    LPBank = 970449,
+    /**
+     * NH TMCP Quân đội
+     */
+    MB_Bank = 970422,
+    /**
+     * NH TMCP Hàng Hải
+     */
+    MSB = 970426,
+    /**
+     * NH TMCP Quốc Dân
+     */
+    NCB = 970419,
+    /**
+     * NH TMCP Nam Á
+     */
+    Nam_A_Bank = 970428,
+    /**
+     * NH Nonghyup - CN Hà Nội
+     */
+    NongHyup_Bank = 801011,
+    /**
+     * NH TMCP Phương Đông
+     */
+    OCB = 970448,
+    /**
+     * NH Thương mại TNHH MTV Đại Dương
+     */
+    Ocean_Bank = 970414,
+    /**
+     * NH TMCP Thịnh vượng và Phát triển
+     */
+    PGBank = 970430,
+    /**
+     * NH TMCP Đại Chúng Việt Nam
+     */
+    PVcomBank = 970412,
+    /**
+     * NH TNHH MTV Public Việt Nam
+     */
+    Public_Bank_Vietnam = 970439,
+    /**
+     * NH TMCP Sài Gòn
+     */
+    SCB = 970429,
+    /**
+     * NH TMCP Sài Gòn - Hà Nội
+     */
+    SHB = 970443,
+    /**
+     * NH TMCP Sài Gòn Thương Tín
+     */
+    Sacombank = 970403,
+    /**
+     * NH TMCP Sài Gòn Công Thương
+     */
+    Saigon_Bank = 970400,
+    /**
+     * NH TMCP Đông Nam Á
+     */
+    SeABank = 970440,
+    /**
+     * NH TNHH MTV Shinhan Việt Nam
+     */
+    Shinhan_Bank = 970424,
+    /**
+     * NH TNHH MTV Standard Chartered Bank Việt Nam
+     */
+    Standard_Chartered_Vietnam = 970410,
+    /**
+     * NH số TNEX
+     */
+    TNEX = 9704261,
+    /**
+     * NH TMCP Tiên Phong
+     */
+    TPBank = 970423,
+    /**
+     * NH TMCP Kỹ thương Việt Nam
+     */
+    Techcombank = 970407,
+    /**
+     * NH số Timo by Bản Việt Bank
+     */
+    Timo = 963388,
+    /**
+     * NH số UBank by VPBank
+     */
+    UBank = 546035,
+    /**
+     * NH United Overseas Bank Việt Nam
+     */
+    United_Overseas_Bank_Vietnam = 970458,
+    /**
+     * NH TMCP Quốc tế Việt Nam
+     */
+    VIB = 970441,
+    /**
+     * NH TMCP Việt Nam Thịnh Vượng
+     */
+    VPBank = 970432,
+    /**
+     * NH Liên doanh Việt - Nga
+     */
+    VRB = 970421,
+    /**
+     * NH TMCP Việt Á
+     */
+    VietABank = 970427,
+    /**
+     * NH TMCP Việt Nam Thương Tín
+     */
+    VietBank = 970433,
+    /**
+     * NH TMCP Ngoại Thương Việt Nam
+     */
+    Vietcombank = 970436,
+    /**
+     * NH TMCP Công thương Việt Nam
+     */
+    VietinBank = 970415,
+    /**
+     * NH TNHH MTV Woori Việt Nam
+     */
+    Woori_Bank = 970457,
 }
