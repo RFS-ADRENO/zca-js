f7b3a1ce2b70f93da438aef32cde920160aa4eef
RFS-ADRENO <dungto76@gmail.com>
2025-09-12 18:45:53 +0700

    feat: export context types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "./Errors/index.js";
 export * from "./models/index.js";
 export * from "./zalo.js";
 
+export type { ContextSession, ContextBase, AppContextExtended, AppContextBase, Options, ZPWServiceMap, ImageMetadataGetter,ImageMetadataGetterResponse } from "./context.js";
+
 // API TYPES
 export type { AcceptFriendRequestResponse } from "./apis/acceptFriendRequest.js";
 export type { AddGroupBlockedMemberResponse } from "./apis/addGroupBlockedMember.js";
