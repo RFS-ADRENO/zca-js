c270dbee96d92c994730fa86a35eddce97cfd8b7
Grosse <77768272+RFS-ADRENO@users.noreply.github.com>
2025-11-14 16:59:47 +0700

    refactor(logout): change response type to unknown; clean up comments

--- src/apis/logout.ts
+++ src/apis/logout.ts
@@ -1,26 +1,17 @@
 import { ZaloApiError } from "../Errors/ZaloApiError.js";
 import { apiFactory } from "../utils.js";
 
-export type LogoutResponse = {
-    data?: unknown;
-    error?: string;
-    error_code?: number;
-};
+export type LogoutResponse = unknown;
 
 export const logoutFactory = apiFactory<LogoutResponse>()((api, ctx, utils) => {
     /**
      * Logout from Zalo account
      *
-     * Implements the logoutV2 logic from Zalo Web client.
-     * This API has no parameters and uses a fixed endpoint.
-     *
      * @throws {ZaloApiError}
      */
     return async function logout() {
-        // Logout endpoint following Zalo Web client implementation
         const serviceURL = utils.makeURL("https://wpa.chat.zalo.me/api/v2/login/logOut");
 
-        // Prepare logout parameters as per Zalo Web client
         const params = {
             time: Date.now(),
             imei: ctx.imei,
