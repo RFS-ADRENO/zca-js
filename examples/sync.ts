c3c6faa4f42ca2584d707b54d7824739f17e341a
RodrickSia <doquocduy698@gmail.com>
2026-02-09 15:00:00 +0700

    Chore: Add in commet of clue about what to handle next

--- examples/sync.ts
+++ examples/sync.ts
@@ -10,6 +10,8 @@ api.listener.on("sync_event", (event) => {
         console.log("Sync data received you can downloaded the file and decrypte the encrypted key", event);
     }
     // TODO handle the decryption of the encrypted key and download the file
+    // Note that the encrypted key can be decrypted using the private key generated from generateRSAKeyPair function I include the decryptWithPrivateKey function
+    api.listener.stop();
 });
 const { publicKeyBase64 } = generateRSAKeyPair();
 api.pullMobile({
