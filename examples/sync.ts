29d01c4732391cf1b68a6630cd715199ef46348a
RodrickSia <doquocduy698@gmail.com>
2026-02-19 22:42:29 +0700

    chore: Clean up the example sync file

--- examples/sync.ts
+++ examples/sync.ts
@@ -7,26 +7,28 @@ const api = await zalo.loginQR();
 
 api.listener.on("sync_event", async (event) => {
     if (event.type == SyncEventType.SYNCMSG_INFO) {
-        console.log("Sync data received you can downloaded the file and decrypte the encrypted key", event);
+        try {
+            // This get the file and also save it to the specified path, you can also handle the file stream if you want
+            const result = await api.getSyncData({
+                url: event.data.url,
+                encrypted_key: event.data.encrypted_key,
+                file_name: event.data.file_name,
+                file_size: event.data.file_size,
+                checksum_code: event.data.checksum_code,
+                from_seq_id: event.data.from_seq_id,
+                is_full_transfer: event.data.is_full_transfer,
+                savePath: "./sync_data",
+            });
 
-        const result = await api.getSyncData({
-            url: event.data.url,
-            encrypted_key: event.data.encrypted_key,
-            file_name: event.data.file_name,
-            file_size: event.data.file_size,
-            checksum_code: event.data.checksum_code,
-            from_seq_id: event.data.from_seq_id,
-            is_full_transfer: event.data.is_full_transfer,
-            savePath: "./sync_data",
-        });
-
-        if (result.success && result.data) {
-            console.log("File saved to:", result.data.file_path);
-            console.log("Encrypted key:", result.data.encrypted_key);
+            if (result.success && result.data) {
+                console.log("File saved to:", result.data.file_path);
+                console.log("Encrypted key:", result.data.encrypted_key);
+            }
+        } catch (error) {
+            console.error("Error syncing data:", error);
         }
     }
-    // TODO handle the decryption of the encrypted key and download the file
-    // Note that the encrypted key can be decrypted using the private key generated from generateRSAKeyPair function I include the decryptWithPrivateKey function
+    api.listener.stop();
 });
 const { publicKeyBase64 } = generateRSAKeyPair();
 api.pullMobile({
