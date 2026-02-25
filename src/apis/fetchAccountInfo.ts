a15e48e0e7b1f3ac94e6c5ff10bde54f6a59de0c
Toan Doan <toandev.95@gmail.com>
2026-02-25 23:33:06 +0700

    fix(fetchAccountInfo): update FetchAccountInfoResponse type to include profile object

--- src/apis/fetchAccountInfo.ts
+++ src/apis/fetchAccountInfo.ts
@@ -2,7 +2,7 @@ import { apiFactory } from "../utils.js";
 
 import type { User } from "../models/index.js";
 
-export type FetchAccountInfoResponse = User;
+export type FetchAccountInfoResponse = { profile: User };
 
 export const fetchAccountInfoFactory = apiFactory<FetchAccountInfoResponse>()((api, _, utils) => {
     const serviceURL = utils.makeURL(`${api.zpwServiceMap.profile[0]}/api/social/profile/me-v2`);
